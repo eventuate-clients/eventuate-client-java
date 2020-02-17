b449b790901d6247e9f74222e2271be1f77cf833
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2020-02-17 12:35:33 +0300

    Improved logging

--- eventuate-client-java-common-impl/src/main/java/io/eventuate/javaclient/commonimpl/adapters/SyncToAsyncAggregateEventsAdapter.java
+++ eventuate-client-java-common-impl/src/main/java/io/eventuate/javaclient/commonimpl/adapters/SyncToAsyncAggregateEventsAdapter.java
@@ -4,6 +4,8 @@ import io.eventuate.CompletableFutureUtil;
 import io.eventuate.SubscriberOptions;
 import io.eventuate.javaclient.commonimpl.SerializedEvent;
 import io.eventuate.javaclient.commonimpl.sync.AggregateEvents;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 import java.util.Set;
@@ -11,6 +13,7 @@ import java.util.concurrent.CompletableFuture;
 import java.util.function.Function;
 
 public class SyncToAsyncAggregateEventsAdapter implements io.eventuate.javaclient.commonimpl.AggregateEvents {
+  private Logger logger = LoggerFactory.getLogger(getClass());
 
   private io.eventuate.javaclient.commonimpl.sync.AggregateEvents target;
 
@@ -21,9 +24,12 @@ public class SyncToAsyncAggregateEventsAdapter implements io.eventuate.javaclien
   @Override
   public CompletableFuture<?> subscribe(String subscriberId, Map<String, Set<String>> aggregatesAndEvents, SubscriberOptions subscriberOptions, Function<SerializedEvent, CompletableFuture<?>> handler) {
     try {
+      logger.info("Subscribing: subscriberId = {}, aggregatesAndEvents = {}, options = {}", subscriberId, aggregatesAndEvents, subscriberOptions);
       target.subscribe(subscriberId, aggregatesAndEvents, subscriberOptions, handler);
+      logger.info("Subscribed: subscriberId = {}, aggregatesAndEvents = {}, options = {}", subscriberId, aggregatesAndEvents, subscriberOptions);
       return CompletableFuture.completedFuture(null);
     } catch (RuntimeException e) {
+      logger.error("Subscription failed", e);
       return CompletableFutureUtil.failedFuture(e);
     }
   }
